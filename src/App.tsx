41bac27eb5c7334082b352a03d480e2827d57149
Felipe Rodrigues <arthurdz741@gmail.com>
2024-04-20 00:58:26 -0300

    Mudando estilização para styled components

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import EstiloGlobal, { Container, theme } from './styles'
+import { ThemeProvider } from 'styled-components'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <EstiloGlobal />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
